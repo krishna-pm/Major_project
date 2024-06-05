1d3ab59b8fbdd568d2bb5665ecf1f27860cab40f
Krishna P M <krishna00murali@gmail.com>
2024-06-05 10:51:39 +0530

    Add files via upload

--- voice/audio.py
+++ voice/audio.py
@@ -1,1 +1,9 @@
+import opensmile
+print("imported")
 
+smile = opensmile.Smile(
+    feature_set=opensmile.FeatureSet.ComParE_2016,
+    feature_level=opensmile.FeatureLevel.Functionals,
+)
+y = smile.process_file('/home/abhijith/Documents/Abijith dataset/ADReSSo21/diagnosis/train/audio/ad/adrso024.wav')
+print(y)
